48fcc949acc8bbff879c7b669f4678d9c535b8ab
RoKenshi <rouronikenshi@gmail.com>
2022-10-25 00:55:15 +0300

    Initial commit

---
+++ src/main/java/com/dmitrypopov/spring/mvc_hibernate_aop/dao/EmployeeDAO.java
@@ -0,0 +1,10 @@
+package com.dmitrypopov.spring.mvc_hibernate_aop.dao;
+
+import com.dmitrypopov.spring.mvc_hibernate_aop.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeDAO {
+    public List<Employee> getAllEmployees();
+
+}
